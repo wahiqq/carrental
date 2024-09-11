400a195fe716e950191b657fe77c9f2746f83c3e
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:23:04 +0530

    fixed the parse error bug

--- show_reservations.php
+++ show_reservations.php
@@ -19,4 +19,19 @@ if ($result->num_rows > 0) {
     echo "<table border='1'>
     <tr>
     <th>BookingID</th>
-    <th>BookingDate</
+    <th>BookingDate</th>
+    </tr>";
+    
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+        <td>" . $row["BookingID"]. "</td>
+        <td>" . $row["BookingDate"]. "</td>
+        </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
