3c23128e66a3bd61f4dfbbb9e96d3dea1decd5a3
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:34:00 +0530

    new code written

--- add_vehicle.php
+++ add_vehicle.php
@@ -12,21 +12,28 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$carID = $_POST['carID'];
-$model = $_POST['model'];
-$manufacturer = $_POST['manufacturer'];
-$yearOfMake = $_POST['yearOfMake'];
-$categoryID = $_POST['categoryID'];
-$locationID = $_POST['locationID'];
-$availabilityStatus = $_POST['availabilityStatus'];
+// Check if POST variables are set
+if (isset($_POST['carID'], $_POST['model'], $_POST['manufacturer'], $_POST['yearOfMake'], $_POST['categoryID'], $_POST['locationID'], $_POST['availabilityStatus'])) {
+    $carID = $conn->real_escape_string($_POST['carID']);
+    $model = $conn->real_escape_string($_POST['model']);
+    $manufacturer = $conn->real_escape_string($_POST['manufacturer']);
+    $yearOfMake = $conn->real_escape_string($_POST['yearOfMake']);
+    $categoryID = $conn->real_escape_string($_POST['categoryID']);
+    $locationID = $conn->real_escape_string($_POST['locationID']);
+    $availabilityStatus = $conn->real_escape_string($_POST['availabilityStatus']);
 
-$sql = "INSERT INTO Car (CarID, Model, Manufacturer, YearOfMake, CategoryID, LocationID, AvailabilityStatus)
-VALUES ('$carID', '$model', '$manufacturer', $yearOfMake, '$categoryID', '$locationID', '$availabilityStatus')";
+    $sql = "INSERT INTO Car (CarID, Model, Manufacturer, YearOfMake, CategoryID, LocationID, AvailabilityStatus)
+    VALUES ('$carID', '$model', '$manufacturer', '$yearOfMake', '$categoryID', '$locationID', '$availabilityStatus')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New vehicle added successfully";
+    if ($conn->query($sql) === TRUE) {
+        echo "New vehicle added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: Missing required POST variables.<br>";
+    echo "Received POST variables:<br>";
+    print_r($_POST);
 }
 
 $conn->close();
