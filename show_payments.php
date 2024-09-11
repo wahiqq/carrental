5cdbbd3401a684645dea434650dd51ee8691a4a5
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:18:15 +0530

    updated show payments file

--- show_payments.php
+++ show_payments.php
@@ -1,45 +1,44 @@
 <?php
-// show_payments.php
-
-// Database connection
 $servername = "localhost";
 $username = "root";
-$password = "924470";
-$database = "car_rental_db";
+$password = "";
+$dbname = "car_rental_db";
 
-$conn = new mysqli($servername, $username, $password, $database);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to fetch all payments
-$sql = "SELECT * FROM Payment";
+$sql = "SELECT * FROM Billing";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Output data of each row
     echo "<table border='1'>
-            <tr>
-                <th>Payment ID</th>
-                <th>Reservation ID</th>
-                <th>Payment Date</th>
-                <th>Amount</th>
-                <th>Payment Method</th>
-            </tr>";
-    while ($row = $result->fetch_assoc()) {
+    <tr>
+    <th>BillID</th>
+    <th>BillDate</th>
+    <th>BillStatus</th>
+    <th>TotalAmount</th>
+    <th>TaxAmount</th>
+    <th>BookingID</th>
+    </tr>";
+    
+    while($row = $result->fetch_assoc()) {
         echo "<tr>
-                <td>" . $row["PaymentID"] . "</td>
-                <td>" . $row["ReservationID"] . "</td>
-                <td>" . $row["PaymentDate"] . "</td>
-                <td>" . $row["Amount"] . "</td>
-                <td>" . $row["PaymentMethod"] . "</td>
-              </tr>";
+        <td>".$row["BillID"]."</td>
+        <td>".$row["BillDate"]."</td>
+        <td>".$row["BillStatus"]."</td>
+        <td>".$row["TotalAmount"]."</td>
+        <td>".$row["TaxAmount"]."</td>
+        <td>".$row["BookingID"]."</td>
+        </tr>";
     }
     echo "</table>";
 } else {
-    echo "No payments found.";
+    echo "0 results";
 }
 
 $conn->close();
