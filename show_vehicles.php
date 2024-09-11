8cb00553710b8396786bb1cbd3bf6ecd16bbf32c
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:24:08 +0530

    fixed the parse error bug

--- show_vehicles.php
+++ show_vehicles.php
@@ -16,6 +16,31 @@ $sql = "SELECT * FROM Car";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table border='1'><tr><th>Car ID</th><th>Model</th><th>Manufacturer</th><th>Year Of Make</th><th>Category ID</th><th>Location ID</th><th>Availability Status</th></tr>";
+    echo "<table border='1'>
+    <tr>
+    <th>Car ID</th>
+    <th>Model</th>
+    <th>Manufacturer</th>
+    <th>Year Of Make</th>
+    <th>Category ID</th>
+    <th>Location ID</th>
+    <th>Availability Status</th>
+    </tr>";
+    
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["CarID"]."</td><td>".$row["Model"]."</td><td>".$row["Manufacturer"]."</td><td>".$row["YearOfMake"]."</td><td>".$row["CategoryID"]."</td><td>".$row
+        echo "<tr>
+        <td>".$row["CarID"]."</td>
+        <td>".$row["Model"]."</td>
+        <td>".$row["Manufacturer"]."</td>
+        <td>".$row["YearOfMake"]."</td>
+        <td>".$row["CategoryID"]."</td>
+        <td>".$row["LocationID"]."</td>
+        <td>".$row["AvailabilityStatus"]."</td>
+        </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
