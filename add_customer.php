64d20fdf6c157be2b816337a1f84a34a2bc91cd7
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:13:01 +0530

    update the add customer file

--- add_customer.php
+++ add_customer.php
@@ -1,9 +1,8 @@
 <?php
-// Database Information
 $servername = "localhost";
-$username = "root";     
-$password = "924470";        
-$dbname = "car_rental_db"; 
+$username = "root";
+$password = "";
+$dbname = "car_rental_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,20 +12,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$address = $_POST['address'];
-$phoneNumber = $_POST['phoneNumber'];
+$customerID = $_POST['customerID'];
+$fullName = $_POST['fullName'];
+$phoneNo = $_POST['phoneNo'];
 $email = $_POST['email'];
-$licenseNumber = $_POST['licenseNumber'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
 
-// Insert new customer into the Customer table
-$sql = "INSERT INTO Customer (FirstName, LastName, Address, PhoneNumber, Email, LicenseNumber)
-        VALUES ('$firstName', '$lastName', '$address', '$phoneNumber', '$email', '$licenseNumber')";
+$sql = "INSERT INTO Customer (CustomerID, FullName, PhoneNo, Email, Address, City, State)
+VALUES ('$customerID', '$fullName', '$phoneNo', '$email', '$address', '$city', '$state')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New customer added successfully!";
+    echo "New customer added successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
