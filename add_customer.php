a6f4100adf0d159b8851b90a28a6a5faac9429e5
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:33:51 +0530

    fixed the sql error bug

--- add_customer.php
+++ add_customer.php
@@ -12,21 +12,28 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$customerID = $_POST['customerID'];
-$fullName = $_POST['fullName'];
-$phoneNo = $_POST['phoneNo'];
-$email = $_POST['email'];
-$address = $_POST['address'];
-$city = $_POST['city'];
-$state = $_POST['state'];
+// Check if POST variables are set
+if (isset($_POST['customerID'], $_POST['fullName'], $_POST['phoneNo'], $_POST['email'], $_POST['address'], $_POST['city'], $_POST['state'])) {
+    $customerID = $conn->real_escape_string($_POST['customerID']);
+    $fullName = $conn->real_escape_string($_POST['fullName']);
+    $phoneNo = $conn->real_escape_string($_POST['phoneNo']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $address = $conn->real_escape_string($_POST['address']);
+    $city = $conn->real_escape_string($_POST['city']);
+    $state = $conn->real_escape_string($_POST['state']);
 
-$sql = "INSERT INTO Customer (CustomerID, FullName, PhoneNo, Email, Address, City, State)
-VALUES ('$customerID', '$fullName', '$phoneNo', '$email', '$address', '$city', '$state')";
+    $sql = "INSERT INTO Customer (CustomerID, FullName, PhoneNo, Email, Address, City, State)
+    VALUES ('$customerID', '$fullName', '$phoneNo', '$email', '$address', '$city', '$state')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New customer added successfully";
+    if ($conn->query($sql) === TRUE) {
+        echo "New customer added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: Missing required POST variables.<br>";
+    echo "Received POST variables:<br>";
+    print_r($_POST);
 }
 
 $conn->close();
