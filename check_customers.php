1a280c8b7c21931321bd2e95edb20c661c31d57c
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:16:21 +0530

    updated check customer file

--- check_customers.php
+++ check_customers.php
@@ -1,9 +1,8 @@
 <?php
-// Database credentials
 $servername = "localhost";
-$username = "root";       
-$password = "924470";      
-$dbname = "car_rental_db"; 
+$username = "root";
+$password = "";
+$dbname = "car_rental_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,14 +12,31 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to fetch all customers
 $sql = "SELECT * FROM Customer";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<h2>Customer List</h2><table border='1'><tr><th>ID</th><th>FullName</th><th>Address</th><th>Phone Number</th><th>Email</th><th>License Number</th></tr>";
+    echo "<table border='1'>
+    <tr>
+    <th>CustomerID</th>
+    <th>FullName</th>
+    <th>PhoneNo</th>
+    <th>Email</th>
+    <th>Address</th>
+    <th>City</th>
+    <th>State</th>
+    </tr>";
+    
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>{$row['CustomerID']}</td><td>{$row['FullName']}</td><td>{$row['Address']}</td><td>{$row['PhoneNumber']}</td><td>{$row['Email']}</td><td>{$row['LicenseNumber']}</td></tr>";
+        echo "<tr>
+        <td>".$row["CustomerID"]."</td>
+        <td>".$row["FullName"]."</td>
+        <td>".$row["PhoneNo"]."</td>
+        <td>".$row["Email"]."</td>
+        <td>".$row["Address"]."</td>
+        <td>".$row["City"]."</td>
+        <td>".$row["State"]."</td>
+        </tr>";
     }
     echo "</table>";
 } else {
