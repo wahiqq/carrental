9f72827414da94e6b9b26c6be7e92fee8076d212
wahiqq <iamwahiqiqbal@gmail.com>
2024-09-12 04:20:25 +0530

    updated show vehicles file

--- show_vehicles.php
+++ show_vehicles.php
@@ -1,8 +1,7 @@
 <?php
-// Database credentials
 $servername = "localhost";
-$username = "root";     
-$password = "924470";     
+$username = "root";
+$password = "";
 $dbname = "car_rental_db";
 
 // Create connection
@@ -13,30 +12,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to fetch all vehicles
-$sql = "SELECT v.VehicleID, v.Make, v.Model, v.Year, v.LicensePlate, vt.TypeName, v.Status, v.Location
-        FROM Vehicle v
-        JOIN VehicleType vt ON v.TypeID = vt.TypeID";
+$sql = "SELECT * FROM Car";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<h2>Vehicle List</h2><table border='1'><tr><th>ID</th><th>Make</th><th>Model</th><th>Year</th><th>License Plate</th><th>Type</th><th>Status</th><th>Location</th></tr>";
+    echo "<table border='1'><tr><th>Car ID</th><th>Model</th><th>Manufacturer</th><th>Year Of Make</th><th>Category ID</th><th>Location ID</th><th>Availability Status</th></tr>";
     while($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>{$row['VehicleID']}</td>
-                <td>{$row['Make']}</td>
-                <td>{$row['Model']}</td>
-                <td>{$row['Year']}</td>
-                <td>{$row['LicensePlate']}</td>
-                <td>{$row['TypeName']}</td>
-                <td>{$row['Status']}</td>
-                <td>{$row['Location']}</td>
-              </tr>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
-
-$conn->close();
-?>
+        echo "<tr><td>".$row["CarID"]."</td><td>".$row["Model"]."</td><td>".$row["Manufacturer"]."</td><td>".$row["YearOfMake"]."</td><td>".$row["CategoryID"]."</td><td>".$row
